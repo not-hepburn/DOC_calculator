15f94b60a75f3c7ccff7f12269e92ea5491ad5f5
Audrey Traore <audrey.traorea@gmail.com>
2025-11-25 05:40:48 +0000

    Worked on the user's data. Unfinished, I need to make the Wang Landau Algorithm and maybe add another option to calculate DOS with the intensity... like STM/STS dI/DV inv prop DOS(E)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("DOS Calculator")
+
+model = st.selectbox("Model", ["Free electron", "1D Chain", "2D square Lattice", "1D phonons"])
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
